27489d8575a3b5042015996f6c38b10805debfea
Cuni <ahet1017@gmail.com>
2023-02-05 20:32:51 +0800

    setup

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="text-red-500">
+      App
+      <Button variant="outlined">Hi</Button>
+    </div>
+  );
+}
+
+export default App;
