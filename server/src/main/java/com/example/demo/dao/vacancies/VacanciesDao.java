075ec0eee32afd9d294bb7b04ac3bed8cf4df9e4
mikeliu900106 <mikeliu20010106@gmail.com>
2023-04-24 13:57:47 +0800

    SKILL

--- server/src/main/java/com/example/demo/dao/vacancies/VacanciesDao.java
+++ server/src/main/java/com/example/demo/dao/vacancies/VacanciesDao.java
@@ -113,6 +113,24 @@ public class VacanciesDao {
         System.out.println(sql);
         return namedParameterJdbcTemplate.query(sql,map,new CompanyVacanciesRowMapper());
     }
+    public List<CompanyVacanciesDto> findApplyVacanciesIdByApplyUpdateTime(String fiveDays){
+        String sql ="SELECT c.company_id, c.company_name, c.company_image_url,\n"+
+                "v.vacancies_id, v.teacher_id, v.vacancies_name, v.vacancies_time, v.vacancies_description,v.vacancies_requirement,\n"+
+                "v.vacancies_work_experience,v.vacancies_district ,v.vacancies_Education, v.vacancies_department,\n"+
+                "v.vacancies_quantity, v.vacancies_create_time, v.apply_count,v.teacher_valid_type,v.vacancies_watch_type,\n"+
+                "group_concat(DISTINCT s.skill_name) skills, group_concat(DISTINCT ct.county_name) county,\n"+
+                "v.vacancies_view,v.vacancies_down_salary,v.vacancies_top_salary,v.vacancies_salary_type\n"+
+                "FROM vacancies v INNER JOIN company c ON c.company_id = v.company_id \n"+
+                "INNER JOIN vacancies_skill vs ON vs.vacancies_id = v.vacancies_id \n"+
+                "INNER JOIN skill s  ON s.skill_id = vs.skill_id \n"+
+                "INNER JOIN vacancies_county vc  ON vc.vacancies_id = v.vacancies_id \n"+
+                "INNER JOIN county ct  ON ct.county_id = vc.county_id \n"+
+                "WHERE 1=1  AND v.vacancies_update_time > :fiveDays";
+        Map<String,Object> map= new HashMap<>();
+        map.put("search",fiveDays);
+        System.out.println(sql);
+        return namedParameterJdbcTemplate.query(sql,map,new CompanyVacanciesRowMapper());
+    }
 
     public Integer findPageVacanciesCount(List<String> county,List<String> technology, String salaryType, Long salaryMax, int salaryMin, String order,int selectLimit, int selectOffset){
         String sql ="SELECT c.company_id, c.company_name, c.company_image_url,\n"+
